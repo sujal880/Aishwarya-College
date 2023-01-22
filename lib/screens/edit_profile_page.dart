2d39311fac6234cfb8041652b06a764665049364
sujaldave880 <sujaldave880@gmail.com>
2023-01-22 17:30:35 +0530

    initial commit

--- lib/screens/edit_profile_page.dart
+++ lib/screens/edit_profile_page.dart
@@ -1,178 +1,230 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../models/usermodel.dart';
+
 class Edit_Profile extends StatefulWidget {
   final UserModel usermodel;
   final User firebaseuser;
-  Edit_Profile({required this.usermodel,required this.firebaseuser});
+  Edit_Profile({required this.usermodel, required this.firebaseuser});
   @override
   State<Edit_Profile> createState() => _Edit_ProfileState();
 }
-
 class _Edit_ProfileState extends State<Edit_Profile> {
-  bool newname=true;
-  bool newmail=true;
-  bool newphone=true;
-  bool newcourse=true;
-  TextEditingController newnameController=TextEditingController();
-  TextEditingController newmailController=TextEditingController();
-  TextEditingController newphoneController=TextEditingController();
-  TextEditingController newcourseController=TextEditingController();
+  late UserModel me;
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(resizeToAvoidBottomInset: false,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Edit Profile"),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 40),
-          Stack(
-            children:[
+      body: Form(
+        key: _formkey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 40),
+            Stack(children: [
               Card(
-                elevation:20,
+                elevation: 20,
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100)
-                ),
+                    borderRadius: BorderRadius.circular(100)),
                 child: CircleAvatar(
                   radius: 70,
                   backgroundColor: Colors.white,
                 ),
               ),
               Padding(
-                padding: const EdgeInsetsDirectional.only(top: 10,start: 10),
+                padding: const EdgeInsetsDirectional.only(top: 10, start: 10),
                 child: CircleAvatar(
                   radius: 65,
-                  backgroundImage: NetworkImage("${widget.usermodel.profilepic}"),
+                  backgroundImage:
+                      NetworkImage("${widget.usermodel.profilepic}"),
                 ),
               ),
               Padding(
-                padding: const EdgeInsetsDirectional.only(start:100,top: 115),
-                child: Icon(Icons.add_circle,size: 30,color: Colors.blue,),
-              )
-            ]
-          ),
-          SizedBox(height: 50),
-          Row(
-            children: [
-              SizedBox(width: 30),
-              Icon(Icons.person,size: 30,color: Colors.blue,),
-              SizedBox(width: 20),
-              Text("Your Name",style: TextStyle(fontSize: 20,color: Colors.grey,),)
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start:85,end: 20),
-            child: TextField(
-              controller: newnameController,
-              decoration: InputDecoration(
-                hintText: '${widget.usermodel.fullname}'
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              SizedBox(width: 30),
-              Icon(Icons.mail,size: 30,color: Colors.blue,),
-              SizedBox(width: 20),
-              Text("Your Mail",style: TextStyle(fontSize: 20,color: Colors.grey,),)
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start:85,end: 20),
-            child: TextField(
-              controller: newmailController,
-              decoration: InputDecoration(
-                  hintText: '${widget.usermodel.email}'
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              SizedBox(width: 30),
-              Icon(Icons.phone_android_outlined,size: 30,color: Colors.blue,),
-              SizedBox(width: 20),
-              Text("Your Phone",style: TextStyle(fontSize: 20,color: Colors.grey,),)
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start:85,end: 20),
-            child: TextField(
-              controller: newphoneController,
-              decoration: InputDecoration(
-                  hintText: '+91${widget.usermodel.phone}'
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              SizedBox(width: 30),
-              Icon(Icons.school,size: 30,color: Colors.blue,),
-              SizedBox(width: 20),
-              Text("Your Course",style: TextStyle(fontSize: 20,color: Colors.grey,),)
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start:85,end: 20),
-            child: TextField(
-              controller: newcourseController,
-              decoration: InputDecoration(
-                  hintText: '${widget.usermodel.course}'
-              ),
-            ),
-          ),
-          SizedBox(height: 40),
-          Card(
-            elevation: 15,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(14)
-            ),
-            child: GestureDetector(
-              onTap: (){
-                updateProfileData();
-              },
-              child: Container(
-                height: 59,
-                width: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.blue
+                padding: const EdgeInsetsDirectional.only(start: 100, top: 115),
+                child: Icon(
+                  Icons.add_circle,
+                  size: 30,
+                  color: Colors.blue,
                 ),
-                child: Center(child: Text("Save",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),),
+              )
+            ]),
+            SizedBox(height: 50),
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Icon(
+                  Icons.person,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Your Name",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 85, end: 20),
+              child: TextFormField(
+                onSaved: (value) => me.fullname = value ?? "",
+                validator: (value) => value != null && value.isNotEmpty
+                    ? null
+                    : "Please Fill Required Fields",
+                initialValue: widget.usermodel.fullname,
+                decoration: InputDecoration(hintText: 'Name'),
               ),
             ),
-          )
-        ],
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Icon(
+                  Icons.mail,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Your Mail",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 85, end: 20),
+              child: TextFormField(
+                onSaved: (value) => me.email = value ?? "",
+                validator: (value) => value != null && value.isNotEmpty
+                    ? null
+                    : "Please Fill Required Fields",
+                initialValue: widget.usermodel.email,
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Icon(
+                  Icons.phone_android_outlined,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Your Phone",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 85, end: 20),
+              child: TextFormField(
+                onSaved: (value) => me.phone = value ?? "",
+                validator: (value) => value != null && value.isNotEmpty
+                    ? null
+                    : "Please Fill Required Fields",
+                initialValue: widget.usermodel.phone,
+                decoration: InputDecoration(hintText: 'Phone'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 30),
+                Icon(
+                  Icons.school,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Your Course",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 85, end: 20),
+              child: TextFormField(
+                onSaved: (value) => me.course = value ?? "",
+                validator: (value) => value != null && value.isNotEmpty
+                    ? null
+                    : "Please Enter Required Fields",
+                initialValue: widget.usermodel.course,
+                decoration: InputDecoration(hintText: 'Course'),
+              ),
+            ),
+            SizedBox(height: 40),
+            Card(
+              elevation: 15,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14)),
+              child: GestureDetector(
+                onTap: () {
+                  if (_formkey.currentState!.validate()) {
+                    _formkey.currentState!.save();
+                    updateUserInfo().then((value) {
+                      log("Profile Updated");
+                    });
+                  }
+                },
+                child: Container(
+                  height: 59,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue),
+                  child: Center(
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
-  updateProfileData(){
-    setState(() {
-      String Newname=newnameController.text.trim();
-      String NewMail=newmailController.text.trim();
-      String NewPhone=newphoneController.text.trim();
-      String NewCourse=newcourseController.text.trim();
-      if(Newname=="" || NewMail=="" || NewPhone=="" || NewCourse==""){
-        showDialog(context: context, builder: (BuildContext context){
-          return AlertDialog(
-            title: Text("Enter Details"),
-            actions: [
-              TextButton(onPressed: (){
-                Navigator.pop(context);
-              }, child: Text("Ok"))
-            ],
-          );
-        });
-      }
-       else{
-         print("done");
-       }
+
+  Future<void> updateUserInfo() async {
+    await FirebaseFirestore.instance
+        .collection("students")
+        .doc(widget.usermodel.email)
+        .update({
+      "fullname": me.fullname,
+      "email": me.email,
+      "phone": me.phone,
+      "course": me.course
     });
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:aishwarya_college/screens/homescreen.dart';
 import 'package:aishwarya_college/screens/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -21,23 +23,20 @@ class Splash extends StatefulWidget{
 
 class SplashScreen extends State<Splash>{
 final getStorge=GetStorage();
-  @override
+@override
   void initState() {
-    Timer(Duration(seconds: 5), () {
-      if(getStorge.read("email")!=null){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>Bottom(userModel: widget.userModel,
-            firebaseuser: widget.firebaseuser)));
+    super.initState();
+    Future.delayed(Duration(milliseconds: 3000),(){
+      if(FirebaseAuth.instance.currentUser!=null){
+        log("${FirebaseAuth.instance.currentUser!.uid}");
+        Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>Bottom(userModel: widget.userModel, firebaseuser: widget.firebaseuser)));
       }
       else{
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>LogIn()));
+        Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>LogIn()));
       }
-    }
-    );
-    super.initState();
+    });
   }
-
   @override
-
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
@@ -59,17 +58,11 @@ final getStorge=GetStorage();
               isRandSize: true,
               isRandomColor: false,
               connectDots: true,
-              // randColorList: [
-              // Colors.red.withAlpha(210),
-              // Colors.white.withAlpha(210),
-              // Colors.yellow.withAlpha(210),
-              // Colors.green.withAlpha(210),
-              // ],
               enableHover: true,
               hoverColor: Colors.black12,
               hoverRadius: 90,
             ),
-            Center(child: Image.asset('assets/images/WhatsApp Image 2022-08-24 at 8.37.01 AM.jpeg'))
+            Center(child: Image.network('https://github.com/sujal880/College_App/blob/master/assets/images/WhatsApp%20Image%202022-08-24%20at%208.37.01%20AM.jpeg?raw=true'))
           ],
         )
     );
